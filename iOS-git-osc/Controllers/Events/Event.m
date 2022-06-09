b8c0d66f699fea27c0537cac2f8ac50c8b900d9c
torish14k <uanganxinyiw829@163.com>
2022-06-09 09:44:48 +0800

    修复从拥有者进入动态时奔溃问题

--- iOS-git-osc/Controllers/Events/Event.m
+++ iOS-git-osc/Controllers/Events/Event.m
@@ -195,6 +195,7 @@ enum action {
     } else if (actionType == COMMENTED) {
         textView.backgroundColor = [Tools uniformColor];
         NSString *comment = [Tools flattenHTML:[[event.events objectForKey:@"note"] objectForKey:@"note"]];
+        comment = comment.length ? comment : @" ";
         comment = [comment stringByTrimmingCharactersInSet: [NSCharacterSet newlineCharacterSet]];
         [textView setAttributedText:[[NSAttributedString alloc] initWithString:comment attributes:digestAttributes]];
     } else if (actionType == MERGED) {
